d1690c2994de7d2c5ca92ef4db94f4c62e483865
Michael Jalkio <mjalkio@gmail.com>
2017-12-31 10:57:42 -0500

    add get_recipient_data method and do some refactoring

--- part2.py
+++ part2.py
@@ -3,52 +3,71 @@ Tutorial on Python regular expressions with Pandas.
 
 Taken from: https://www.dataquest.io/blog/regular-expressions-data-scientists/
 """
-import email
 import re
+from email import message_from_string as email_message_from_string
 
 import pandas as pd
 
 
+def _parse_name_and_email(line):
+    email = re.search(r"\w\S*@.*\w", line)
+    if email is not None:
+        email = email.group()
+
+    name = re.search(r":.*<", line)
+    if name is not None:
+        # Remove the colon and any whitespace characters
+        name = re.sub(":\s*", "", name.group())
+        # Remove whitespace characters and the angle bracket on the other side
+        name = re.sub("\s*<", "", name)
+        # Remove the quotes too...
+        name = name.strip('"')
+
+    return (name, email)
+
+
 def get_sender_data(fraud_email):
     """Return a dictionary with sender data for the email."""
-    # Step 1: find the whole line beginning with "From:".
-    sender = re.search(r"From:.*", fraud_email)
-
-    # Step 2: find the email address and name.
+    sender = re.search("From:.*", fraud_email)
     if sender is not None:
-        sender_email = re.search(r"\w\S*@.*\w", sender.group())
-        sender_name = re.search(r":.*<", sender.group())
+        sender_name, sender_email = _parse_name_and_email(sender.group())
     else:
-        sender_email = None
-        sender_name = None
-
-    # Step 3A: assign email address as string to a variable.
-    if sender_email is not None:
-        sender_email = sender_email.group()
-
-    # Step 3B: remove unwanted substrings, assign to variable.
-    if sender_name is not None:
-        # Remove the colon and any whitespace characters
-        sender_name = re.sub(":\s*", "", sender_name.group())
-        # Remove whitespace characters and the angle bracket on the other side
-        sender_name = re.sub("\s*<", "", sender_name)
-        # Remove the quotes too...
-        sender_name = sender_name.strip('"')
+        sender_name, sender_email = None, None
 
     return {
         'sender_name': sender_name,
         'sender_email': sender_email,
     }
 
-email_data = []
 
-with open('test_emails.txt', 'r') as f:
-    content = f.read()
+def get_recipient_data(fraud_email):
+    """Return a dictionary with recipient data for the email."""
+    recipient = re.search("To:.*", fraud_email)
+    if recipient is not None:
+        line = recipient.group()
+        recipient_name, recipient_email = _parse_name_and_email(line)
 
-emails = re.split("From r", content)
-emails.pop(0)  # Remove the first empty string
+    return {
+        'recipient_name': recipient_name,
+        'recipient_email': recipient_email,
+    }
 
-for fraud_email in emails:
-    data = {}
-    data.update(get_sender_data(fraud_email))
-    print(data)
+
+def get_date_data(fraud_email):
+    """Return a dictionary with the date data for the email."""
+    return {}
+
+if __name__ == '__main__':
+    email_data = []
+
+    with open('test_emails.txt', 'r') as f:
+        content = f.read()
+
+    emails = re.split("From r", content)
+    emails.pop(0)  # Remove the first empty string
+
+    for fraud_email in emails:
+        data = {}
+        for fn in (get_sender_data, get_recipient_data, get_date_data):
+            data.update(fn(fraud_email))
+        print(data)
